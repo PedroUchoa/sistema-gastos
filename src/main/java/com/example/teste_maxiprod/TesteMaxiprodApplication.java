57977cb2e32fa4ce7bf282baf13f254218b9533d
PedroUchoa <joaopedrouchoacampos@gmail.com>
2025-02-13 18:38:51 -0300

    first commit

---
+++ src/main/java/com/example/teste_maxiprod/TesteMaxiprodApplication.java
@@ -0,0 +1,13 @@
+package com.example.teste_maxiprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteMaxiprodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteMaxiprodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teste_maxiprod/TesteMaxiprodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teste_maxiprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteMaxiprodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
